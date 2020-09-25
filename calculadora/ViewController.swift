262e786c7dcdd7804b3308785dd5e8faf0221e20
pedro <pedro_soto_apps1ta1819@cev.com>
2020-09-25 18:20:55 +0200

    Arreglo vista v2 y action grupal numeros

--- calculadora/ViewController.swift
+++ calculadora/ViewController.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 class ViewController: UIViewController, CalculadoraProtocol {
+    
+    @IBOutlet weak var display: UILabel!
+    
     func suma(num1: Int, num2: Int) -> Int {
         let resultado = num1 + num2
         return resultado
@@ -31,7 +34,56 @@ class ViewController: UIViewController, CalculadoraProtocol {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func numbers(sender: UIButton){
+        switch sender.accessibilityIdentifier {
+            case "zero":
+                display.text! += "0"
+                break
+            
+            case "one":
+                display.text! += "1"
+                break
+            
+            case "two":
+                display.text! += "2"
+                break
+            
+            case "three":
+                display.text! += "3"
+                break
+            
+            case "four":
+                display.text! += "4"
+                break
+            
+            case "five":
+                display.text! += "5"
+                break
+            
+            case "six":
+                display.text! += "6"
+                break
+            
+            case "seven":
+                display.text! += "7"
+                break
+            
+            case "eight":
+                display.text! += "8"
+                break
+            
+            case "nine":
+                display.text! += "9"
+                break
+            
+            
+            default:
+                display.text = "error"
+                break
+        }
+    }
+    
 }
 
 protocol CalculadoraProtocol {
